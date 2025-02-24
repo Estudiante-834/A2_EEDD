37f5d9e8a2337756e0ace68ba3ae4782819a40f2
Estudiante-834 <Estudiante_834@outlook.com>
2025-02-24 16:29:34 +0100

    ADD: Punto entrada Main a otras clases.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,38 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		char opcion;
+		
+		do {
+			System.out.println("Seleccione una opción:");
+			System.out.println("Operaciones Suma (s)");
+			System.out.println("Salir (q)");
+			
+			opcion = sc.next().toLowerCase().charAt(0);
+			
+			switch (opcion) {
+			case 's':
+				Suma.menu(sc);
+				break;
+			case 'r':
+				
+				break;
+			case 'q':
+				break;
+			default:
+				System.out.println("Opción inválida.");
+			}
+			
+		} while (opcion != 'q');
+		
+		sc.close();
+		
 	}
 
 }
